f0a1ed37dd0dc2ced43728596991acc1367caa70
Agustin Flores <agustinflores1505@gmail.com>
2023-08-09 23:40:43 -0300

    Se agrego modal con respuesta a la hora de buscar un personaje y un autocomplete en el buscador.

--- scripts/funciones.js
+++ scripts/funciones.js
@@ -1,4 +1,10 @@
-import { findCharacterInput } from "./index.js";
+import {
+  findCharacterInput,
+  closeModal,
+  modalContainer,
+  parrafoModal,
+  tituloModal,
+} from "./index.js";
 import personajes from "./mock-data.js";
 
 export const obtenerPersonajesFiltradosPorGenero = (
@@ -20,13 +26,44 @@ export const buscarPersonaje = (nombrePersonaje) => {
 };
 
 export const validarExistenciaPersonaje = () => {
+  let seEncontro;
   const nombrePersonajeABuscar = findCharacterInput.value;
   if (nombrePersonajeABuscar != undefined && nombrePersonajeABuscar != "") {
     const personajeRetornado = buscarPersonaje(nombrePersonajeABuscar);
-    personajeRetornado === undefined
-      ? alert("No se encontro el personaje entre los personajes principales.")
-      : alert(
-          `El personaje existe y aqui esta su informacion principal.\n ${personajeRetornado}`
-        );
+    if (personajeRetornado !== undefined) {
+      sessionStorage.setItem(
+        "personajeBuscado",
+        JSON.stringify(personajeRetornado)
+      );
+      seEncontro = true;
+    } else {
+      seEncontro = false;
+    }
+  }
+  return seEncontro;
+};
+
+export const mostrarPopUp = (seEncontro) => {
+  modalContainer.classList.remove("success");
+  closeModal.classList.remove("success");
+  modalContainer.classList.add("error");
+  closeModal.classList.add("error");
+  modalContainer.classList.add("show");
+  let nombreBuscado = findCharacterInput.value;
+
+  if (seEncontro === undefined) {
+    findCharacterInput.value = "";
+    tituloModal.textContent = `Personaje no encontrado!`;
+    parrafoModal.textContent = `No ingreso ningun nombre de personaje a buscar.`;
+  } else if (seEncontro === false) {
+    findCharacterInput.value = "";
+    tituloModal.textContent = `Personaje no encontrado!`;
+    parrafoModal.textContent = `No existe el personaje con nombre '${nombreBuscado}.'`;
+  } else {
+    const personaje = JSON.parse(sessionStorage.getItem("personajeBuscado"));
+    tituloModal.textContent = `Personaje encontrado!`;
+    parrafoModal.textContent = `Status: ${personaje.status}. Specie: ${personaje.species}. Gender: ${personaje.gender}. Origin: ${personaje.origin}.`;
+    modalContainer.classList.add("success");
+    closeModal.classList.add("success");
   }
 };
--- scripts/index.js
+++ scripts/index.js
@@ -2,36 +2,40 @@ import personajes from "./mock-data.js";
 import {
   obtenerPersonajesFiltradosPorGenero,
   validarExistenciaPersonaje,
+  mostrarPopUp,
 } from "./funciones.js";
 
+const findCharacterButton = document.querySelector("#find-character-button");
+const optionsCharacters = document.querySelector("#options-characters");
+
+export const modalContainer = document.querySelector(".modal-container");
+export const closeModal = document.querySelector("#boton-cierre-modal");
+export const parrafoModal = document.querySelector("#parrafo-modal");
+export const tituloModal = document.querySelector("#titulo-modal");
 export const findCharacterInput = document.querySelector(
   "#find-character-input"
 );
-const findCharacterButton = document.querySelector(
-  "#find-character-button"
-);
+
+closeModal.addEventListener("click", () => {
+  modalContainer.classList.remove("show");
+});
 
 findCharacterButton.addEventListener("click", () => {
-  validarExistenciaPersonaje();
+  let seEncontroPersonaje = validarExistenciaPersonaje();
+  mostrarPopUp(seEncontroPersonaje);
 });
 
 findCharacterInput.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
-    validarExistenciaPersonaje();
+    let seEncontroPersonaje = validarExistenciaPersonaje();
+    mostrarPopUp(seEncontroPersonaje);
   }
 });
 
-const GENERO_FEMENINO = "Female";
-const GENERO_MASCULINO = "Male";
-
-const personajesFemeninos = obtenerPersonajesFiltradosPorGenero(
-  personajes,
-  GENERO_FEMENINO
-);
-const personajesMasculinos = obtenerPersonajesFiltradosPorGenero(
-  personajes,
-  GENERO_MASCULINO
-);
-
-console.log(`Los personajes femeninos son: ${personajesFemeninos}.`);
-console.log(`Los personajes masculinos son: ${personajesMasculinos}.`);
+document.addEventListener("DOMContentLoaded", () => {
+  personajes.forEach((p) => {
+    let option = document.createElement("option");
+    option.value = p.name;
+    optionsCharacters.appendChild(option);
+  });
+});
